fcc88312abfe803da4243ba5a5b583aeae200a72
Alexey Radkevich <arr.scout@gmail.com>
2016-09-22 14:11:46 +0300

    AddSort

--- Lab1_QuickSort/Program.cs
+++ Lab1_QuickSort/Program.cs
@@ -10,6 +10,39 @@ namespace Lab1_QuickSort
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+
+    class QuickSort<T> where T : IComparable<T>
+    {
+        public void Sort(T[] arr, int l, int r)
+        {
+            int i = l, j = r;
+            T x = arr[l + (r - l) / 2];
+            while (i <= j)
+            {
+                while (arr[i].CompareTo(x) < 0) i++;
+                while (arr[j].CompareTo(x) > 0) j--;
+
+                if (i <= j)
+                {
+                    // Swap(ref arr[i], ref arr[j]);
+                    i++;
+                    j++;
+                }
+
+                if (l < j)
+                {
+                    //Sort(arr,l,r);
+
+                }
+
+                if (r > i)
+                {
+                    //Sort(arr,l,r);
+                }
+            }
         }
     }
 }
